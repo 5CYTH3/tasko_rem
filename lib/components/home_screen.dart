70d7a11014263dec2d8358e91247bad5918a3862
5CYTH3 <ScytheD@outlook.fr>
2021-06-03 22:10:54 +0200

    Added package google font and created a better Appbar. Trying to do a clean body bruh

--- lib/components/home_screen.dart
+++ lib/components/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Body.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class HomeScreen extends StatelessWidget {
 	@override
@@ -13,11 +15,25 @@ class HomeScreen extends StatelessWidget {
 	AppBar buildAppBar() {
 		return AppBar(
 			elevation: 0,
-			leading: IconButton(icon: Icon(Icons.settings_outlined), onPressed: () {
-
+			leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {
+      
       }),
-			title: Text('Flut Tasko'),
-
+			title: Text('Flut Tasko',
+        style: GoogleFonts.nunito(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        )
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.settings_outlined,
+            color: Colors.white,
+          ),
+          onPressed: null,
+        ),
+      ],
 		);
 	}
 
